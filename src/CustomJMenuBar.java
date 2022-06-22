0965809ef6bc7bef234c172f61896a675f228a0e
George Bacalu <georgebacalu83@gmail.com>
2022-06-22 17:40:12 +0300

    Added key events to menu (items) + started creating logic for controlling whether a file was modified so the user is prompted before trying to close it

--- src/CustomJMenuBar.java
+++ src/CustomJMenuBar.java
@@ -2,8 +2,12 @@ import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,56 +15,82 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class CustomJMenuBar extends JMenuBar {
+public class CustomJMenuBar extends JMenuBar implements ActionListener {
+   private final JMenuItem openItem;
+   private final JMenuItem saveItem;
+   private final JMenuItem exitItem;
+   private final JTextArea textArea;
+   private String fileName;
+   private boolean isChanged;
 
-   public CustomJMenuBar(JTextArea textArea) {
-      JMenuItem openItem = new JMenuItem("Open");
-      openItem.addActionListener((e) -> {
-         JFileChooser fileChooser = new JFileChooser();
-         fileChooser.setCurrentDirectory(new File(".")); // the selected file path is the project root
-         fileChooser.setFileFilter(new FileNameExtensionFilter("Text files", "txt"));
-
-         if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-            File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-            String line;
-            try {
-               BufferedReader reader = new BufferedReader(new FileReader(file));
-               if (file.isFile()){
-                  textArea.setText("");
-                  while ((line = reader.readLine()) != null) textArea.append(line + "\n");
-               }
-               reader.close();
-            } catch (IOException ex) {
-               ex.printStackTrace();
-            }
-         }
-      });
-
-      JMenuItem saveItem = new JMenuItem("Save");
-      saveItem.addActionListener((e) -> {
-         JFileChooser fileChooser = new JFileChooser();
-         fileChooser.setCurrentDirectory(new File("."));
-
-         if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-            File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-            try {
-               PrintWriter writer = new PrintWriter(file);
-               writer.println(textArea.getText());
-               writer.close();
-            } catch (FileNotFoundException ex) {
-               ex.printStackTrace();
-            }
-         }
-      });
-
-      JMenuItem exitItem = new JMenuItem("Exit");
-      exitItem.addActionListener((e) -> System.exit(0));
+   public CustomJMenuBar(JTextArea textArea, String fileName, boolean isChanged) {
+      this.textArea = textArea;
+      this.fileName = fileName;
+      this.isChanged = isChanged;
 
       JMenu fileMenu = new JMenu("File");
-      fileMenu.add(openItem);
-      fileMenu.add(saveItem);
-      fileMenu.add(exitItem);
+      fileMenu.setMnemonic(KeyEvent.VK_F);
+      openItem = new JMenuItem("Open"); openItem.addActionListener(this); openItem.setMnemonic(KeyEvent.VK_O); fileMenu.add(openItem);
+      saveItem = new JMenuItem("Save"); saveItem.addActionListener(this); saveItem.setMnemonic(KeyEvent.VK_S); fileMenu.add(saveItem);
+      exitItem = new JMenuItem("Exit"); exitItem.addActionListener(this); exitItem.setMnemonic(KeyEvent.VK_E); fileMenu.add(exitItem);
 
       this.add(fileMenu);
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      if (e.getSource() == openItem) openFile();
+      else if (e.getSource() == saveItem) saveFile();
+      else if (e.getSource() == exitItem) exitFile();
+   }
+
+   private void openFile() {
+      JFileChooser fileChooser = new JFileChooser();
+      fileChooser.setCurrentDirectory(new File(".")); // the selected file path is the project root
+      fileChooser.setFileFilter(new FileNameExtensionFilter("Text files", "txt"));
+
+      if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+         File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+         String line;
+         try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            if (file.isFile()) {
+               textArea.setText("");
+               isChanged = false;
+               fileName = fileChooser.getSelectedFile().getPath();
+               while ((line = reader.readLine()) != null) textArea.append(line + "\n");
+            }
+            reader.close();
+         } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Error at file opening!", ex.getMessage(), JOptionPane.WARNING_MESSAGE);
+         }
+      }
+   }
+
+   private void saveFile() {
+      JFileChooser fileChooser = new JFileChooser();
+      fileChooser.setCurrentDirectory(new File("."));
+
+      if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+         File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+         try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(textArea.getText());
+            writer.close();
+            isChanged = false;
+         } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error at file writing!", ex.getMessage(), JOptionPane.WARNING_MESSAGE);
+         }
+      }
+   }
+
+   private void exitFile() {
+      if(JOptionPane.showConfirmDialog(
+              null,
+              "Do you want to save modification made to " + fileName + "?",
+              "Save modifications",
+              JOptionPane.YES_NO_CANCEL_OPTION) == JOptionPane.OK_OPTION) {
+         System.exit(0);
+      }
    }
 }
--- src/TextEditor.java
+++ src/TextEditor.java
@@ -5,14 +5,27 @@ import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class TextEditor extends JFrame {
+   private String fileName = "undefined";
+   private boolean isChanged = false;
 
    public TextEditor() {
       JTextArea textArea = new JTextArea();
       textArea.setLineWrap(true);
       textArea.setWrapStyleWord(true);
       textArea.setFont(new Font("Arial", Font.PLAIN, 20));
+      textArea.addKeyListener(new KeyListener() {
+         @Override public void keyTyped(KeyEvent e) {
+            if(e.getKeyCode() > 31 && e.getKeyCode() < 127) isChanged = true;
+         }
+         @Override public void keyPressed(KeyEvent e) {
+         }
+         @Override public void keyReleased(KeyEvent e) {
+         }
+      });
 
       JScrollPane scrollPane = new JScrollPane(textArea);
       scrollPane.setPreferredSize(new Dimension(450, 450));
@@ -20,9 +33,9 @@ public class TextEditor extends JFrame {
 
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       this.setSize(500, 600);
-      this.setTitle("Text editor");
+      this.setTitle(isChanged ? "*" : "" + fileName + " - Custom Text Editor");
       this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-      this.setJMenuBar(new CustomJMenuBar(textArea));
+      this.setJMenuBar(new CustomJMenuBar(textArea, fileName, isChanged));
       this.add(new EditorFunctionsJPanel(textArea));
       this.add(scrollPane);
       this.setLocationRelativeTo(null);
